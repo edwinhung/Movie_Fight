7dbf0a97a6d7d47f071ffe29ca3b769fcf5c20d8
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-06-25 22:15:27 -0700

    add user input for api request

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: OMDB_API,
-      s: "avatar",
+      s: searchTerm,
     },
   });
   console.log(response.data);
 };
 
-fetchData();
+const input = document.body.querySelector("input");
+let timeoutId;
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 1000);
+};
+input.addEventListener("input", onInput);
