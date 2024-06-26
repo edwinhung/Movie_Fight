0aacb61ca47a6d7066e4e48c0f20c2aca102ed97
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-06-26 11:33:06 -0700

    show list of movies and handle error response

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
+  if (response.data.Error) {
+    return [];
+  }
   return response.data.Search;
 };
 
@@ -12,6 +15,14 @@ const input = document.body.querySelector("input");
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
-  console.log(movies);
+  const target = document.body.querySelector("#target");
+  for (let movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <img src="${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+    `;
+    target.append(div);
+  }
 };
 input.addEventListener("input", debounce(onInput, 500));
