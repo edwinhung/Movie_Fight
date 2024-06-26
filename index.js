065a57dad08081dce39579da37bd2b0176a0ca18
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-06-26 11:14:31 -0700

    move debounce to utils.js

--- index.js
+++ index.js
@@ -9,17 +9,6 @@ const fetchData = async (searchTerm) => {
 };
 
 const input = document.body.querySelector("input");
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
 
 const onInput = debounce((event) => {
   fetchData(event.target.value);
